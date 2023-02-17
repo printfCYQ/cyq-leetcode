414b7ff0c44386d0d93c48cb3f5de5e84d32a70a
printfCYQ <459987870@qq.com>
2023-02-17 09:40:38 +0800

    re 442

--- 442.数组中重复的数据.js
+++ 442.数组中重复的数据.js
@@ -10,17 +10,20 @@
  * @return {number[]}
  */
 var findDuplicates = function (nums) {
-  const set = new Set();
-  const res = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (set.has(nums[i])) {
-      res.push(nums[i]);
+  const n = nums.length;
+  const ans = [];
+  for (let i = 0; i < n; ++i) {
+    const x = Math.abs(nums[i]);
+    console.log(nums, x);
+    if (nums[x - 1] > 0) {
+      nums[x - 1] = -nums[x - 1];
     } else {
-      set.add(nums[i]);
+      ans.push(x);
     }
   }
-  return res;
+  return ans;
 };
+
 const nums = [4, 3, 2, 7, 8, 2, 3, 1];
 console.log(findDuplicates(nums));
 // @lc code=end
