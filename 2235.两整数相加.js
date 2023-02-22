35004282880d6171738b6d20f57340a694f4e291
printfCYQ <459987870@qq.com>
2023-02-22 09:31:53 +0800

    2235

---
+++ 2235.两整数相加.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=2235 lang=javascript
+ *
+ * [2235] 两整数相加
+ */
+
+// @lc code=start
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function (num1, num2) {
+  return num1 + num2;
+};
+const num1 = 12,
+  num2 = 5;
+console.log(sum(num1, num2));
+// @lc code=end
