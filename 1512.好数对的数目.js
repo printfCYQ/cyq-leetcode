a04b6f90c0e4e48748904b9ad6a3518bf5212d6a
printfCYQ <459987870@qq.com>
2023-03-13 16:58:19 +0800

    1512 暴力

---
+++ 1512.好数对的数目.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1512 lang=javascript
+ *
+ * [1512] 好数对的数目
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function (nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) count++;
+    }
+  }
+  return count;
+};
+const nums = [1, 2, 3, 1, 1, 3];
+console.log(numIdenticalPairs(nums));
+// @lc code=end
