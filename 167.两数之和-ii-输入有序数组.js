d2454858447c629d32619632173a3edd1974f2e6
printfCYQ <459987870@qq.com>
2023-04-05 21:03:44 +0800

    167 二分

---
+++ 167.两数之和-ii-输入有序数组.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let left = 0,
+    right = numbers.length - 1;
+  while (left < right) {
+    if (numbers[left] + numbers[right] === target) {
+      return [left + 1, right + 1];
+    } else if (numbers[left] + numbers[right] > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
+// @lc code=end
